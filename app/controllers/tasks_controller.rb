69fe6c85b8aeaab793f9e4c14afa35c4aa288ca4
rufunt <jazzoo@mail.ru>
2018-08-18 21:10:23 +0000

    Add Display and Create Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
   
   def index
+    @tasks =Task.all.order("created_at DESC")
   end
   
   def show
@@ -35,10 +36,10 @@ class TasksController < ApplicationController
   private
   
   def tasks_params
-    params_require(:task).permit(:title, :description, :company, :url)
+    params.require(:task).permit(:title, :description, :company, :url)
   end
   
   def find_task
-    @task - Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 end
